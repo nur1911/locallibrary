69aa542a2fb60dfbe6cda3176c385d9eafe6e944
nur1911 <nurgul02081991@gmail.com>
2021-07-17 13:19:23 +0600

    createsuperuser and register models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstanse, Language
+#
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#     fields = ['last_name', 'first_name',('date_of_birth', 'date_of_death')]
+#
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title','author','display_genre')
+#
+# @admin.register(BookInstanse)
+# class BookInstanseAdmin(admin.ModelAdmin):
+#     list_display = ('status','due_back')
+#
+#     fieldsets = (
+#         (None,{
+#             'fields':('book','imprint','id')
+#
+#         }),
+#         ('Availability',{
+#             'feilds':('status', 'due_back')
+#         }),
+#     )
+
+
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register (BookInstanse)
+admin.site.register (Language)
+
--- catalog/models.py
+++ catalog/models.py
@@ -26,6 +26,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_desciption = 'Genre'
+
+
+
+
 
 class BookInstanse(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique id')
