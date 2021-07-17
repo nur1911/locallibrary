6219e6a13082c314f264cf9764e8b63bcc857a90
nur1911 <nurgul02081991@gmail.com>
2021-07-17 14:58:38 +0600

    create htmlfirstsite

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, BookInstanse, Author, Genre
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstanse.objects.all().count()
+    num_instances_available = BookInstanse.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count(2)
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors}
+    )
